efb9cb12c0510ed4dbb5cb3b843ed5a23d58cf54
11yoav <yoavprotter@gmail.com>
2026-01-31 17:09:39 +0200

    did structs and added Final_Calc function

--- Kaki.c
+++ Kaki.c
@@ -1,116 +1,81 @@
 #include <stdio.h>
 #include <math.h>
 
+typedef struct {
+    int num1, num2, num3;
+    char op1, op2;
+    int intake;
+} input;
+
+input GetInput(){
+    input ui  = {0};  
+
+    ui.intake = scanf("%d %c %d", &ui.num1, &ui.op1, &ui.num2);
+    if (ui.intake != 3) return ui;
+
+    char c = getchar(); 
+    if (c != '\n' && c != EOF) {
+            ungetc(c, stdin); 
+            int intake2 = scanf(" %c %d", &ui.op2, &ui.num3);
+            if (intake2 == 2) ui.intake = 5;
+        }
+    return ui;}
 
 double calc(double a, double b, char op, int *err){   
-    double result;
-     *err = 0;
-
-    if (op == '+'){     
-            result = a + b ;}
-        
-    else if (op == '-'){
-            result = a - b;}
-        
-    else if (op == '*'){ 
-            result = a * b;}
-        
-    else if (op == '/'){
-        if (b == 0) {
-                printf("no dividing by 0 ERROR! ");
-                *err = 1;
-                return 0;  }
-        else{
-           result = (double)a / b;}
-        }  
-        
-        
-else  {
-            printf("Bad op!");
-            *err = 1;
-            return 0;}
-    
-    return result;
-}
-
+    *err = 0;
+    switch (op) {
+    case '+': return a + b;
+    case '-': return a - b;
+    case '*': return a * b;   
+    case '/': if (b == 0) {
+        printf("no dividing by 0 ERROR! ");
+        *err = 1;
+        return 0;}
+            
+        return a / b;
+    default: printf("Bad op!"); 
+        *err = 1;
+        return 0;}}
 
 int check(char op1, char op2) {
     int val1 = 0;
     int val2 = 0;
 
-    if (op1 == '+' || op1 == '-') {
-        val1 = 1;
-    }
-    if (op2 == '+' || op2 == '-') {
-        val2 = 1;
-    }
-    if (op1 == '/' || op1 == '*') {
-        val1 = 2;
-    }
-    if (op2 == '/' || op2 == '*') {
-        val2 = 2;
-    }
-
+    if (op1 == '+' || op1 == '-') val1 = 1;
+    if (op2 == '+' || op2 == '-') val2 = 1;
+    if (op1 == '/' || op1 == '*') val1 = 2;
+    if (op2 == '/' || op2 == '*') val2 = 2;
     if (val1 == 0 || val2 == 0) return 0;
+    if (val1 == val2) return 1;
+    if (val1 > val2) return 2;
     
-    if (val1 == val2) {
-        return 1;
-    }
+    else {return 3;}}
 
-    if (val1 > val2) {
-        return 2;
-    } else {
-        return 3;
-    }
-}
+double Final_Calc(input data, int *err) {
+    double res;
+    
+    if (data.intake == 3) return calc(data.num1, data.num2, data.op1, err);
+    
+    int temp = check(data.op1, data.op2);
+    
+    if (temp == 1 || temp == 2) {   
+        res = calc(data.num1, data.num2, data.op1, err);
+        if (*err) return 0;
+        return calc(res, data.num3, data.op2, err);}
+    
+    else {
+        res = calc(data.num2, data.num3, data.op2, err);
+        if (*err) return 0;
+        return calc(data.num1, res, data.op1, err);}}
 
 
 
 int main() {
-    int num1;    int num2;    int num3;
-    char op1;    char op2;
-    double res;      double Fres; int err = 0;
-   
-int intake = scanf("%d %c %d", &num1, &op1, &num2);
-if (intake != 3) return 1;
-
-int intake2 = 0;
-char c = getchar(); 
-    if (c != '\n' && c != EOF) {
-        ungetc(c, stdin); 
-        intake2 = scanf(" %c %d", &op2, &num3);
-    }
-
-if (intake2 == 2) {
-        int temp = check(op1, op2);
-
-        if (temp == 1 || temp == 2) {   
-            res = calc(num1, num2, op1, &err);
-            if (err) return 1;
-
-            Fres = calc(res, num3, op2, &err);
-            if (err) return 1;
-        } 
-        
-        else {                          
-            res = calc(num2, num3, op2, &err);
-            if (err) return 1;
-            Fres = calc(num1, res, op1, &err);
-            if (err) return 1;
-        }
-}    
-
-else {
-        Fres = calc(num1, num2, op1, &err);
-            if (err) return 1;    }
-        
-
-   
-printf("Result: %d\n", (int)round(Fres));
-return 0;
-
-
-}
-
-
-
+    int err = 0; 
+    input myData = GetInput();
+    if (myData.intake < 3)  return 1;
+    double Fres = Final_Calc(myData, &err);
+    if (err) return 1;
+    printf("Result: %d\n", (int)round(Fres));
+    return 0;
+}   
